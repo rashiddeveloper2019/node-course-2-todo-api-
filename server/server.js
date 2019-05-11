b7ce6be82734fbe0334e9ae5305192baf999af27
raghwendra3 <your_email. raghwendrapratap0194@gmail.com>
2019-05-11 13:34:59 +0530

    Add queries playground file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} =require('mongodb')
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -33,6 +34,27 @@ app.get('/todos', (req, res) => {
        res.status(400).send(e);
     })
 }); 
+
+app.get('/todos/:id',(req,res)=> {
+    var id = req.params.id;
+    console.log(id);
+    
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+  
+    Todo.findById(id).then((todo) => {
+        console.log(todo);
+        if(!todo) {
+            return res.status(400).send();
+        }
+
+        res.send({todo});
+        
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, () => {
      console.log('Started on port 3000');
