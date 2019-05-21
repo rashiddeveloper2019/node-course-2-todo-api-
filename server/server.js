b7c4ca3e99001ea7d90d5d8ee24acbf1903c6e56
raghwendra3 <your_email. raghwendrapratap0194@gmail.com>
2019-05-21 06:38:45 +0530

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -118,8 +118,23 @@ app.get('/todos/:id',(req,res)=> {
         res.send(req.user);
     }); 
 
+    // POST /users/login {email, password}
+    app.post('/users/login', (req, res) => {
+        var body = _.pick(req.body, ['email', 'password']);
+
+        User.findByCredentials(body.email, body.password).then((user) => {
+            return user.generateAuthToken().then((token) => {
+                res.header('x-auth',token).send(user);
+            });
+        }).catch((e) => {
+            res.status(400).send();
+        
+    });
+
 app.listen(port, () => {
      console.log(`Started up at port ${port}`);
 });
 
-module.exports = {app};
+module.exports = {app}
+
+});
